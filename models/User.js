ef3a707db8f1c742d981f133279bff68de050b66
Ahmad <ahmdaf420@gmail.com>
2020-12-28 12:09:45 +0200

    Conflict solved

--- models/User.js
+++ models/User.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-});
-
-module.exports = mongoose.model('user', UserSchema);
-=======
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
@@ -53,4 +27,3 @@ const UserSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('user', UserSchema);
->>>>>>> 880a72c0246e082a41ebc7354cea96c3a7e47088
