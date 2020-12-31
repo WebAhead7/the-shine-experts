c710e885d5178e8b0f8e396b885378952c2b515f
nizarhalloun <nshalloun@hotmail.com>
2020-12-31 16:52:42 +0200

    typo

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
-  // Check of not token
+  // Check if not token
   if (!token) {
     return res.status(401).json({ msg: 'No token, auth denied' });
   }
